c44f378ac43eb4d41cef8fc9a3c9ad330f3189e5
AntoineRoulinCPNV <61775794+AntoineRoulinCPNV@users.noreply.github.com>
2020-04-01 16:37:24 +0200

    V0.9
    Fonction login fonctionnel
    Fonction logs
    Fonction enregistrer un score dans un fichier séparer
    Fonction Afficher les scores précédent en allant les chercher dans un fichier séparer
    Fonction chargement de map aléatoire

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 /*
  * Projet : Bataille Navale
  * Auteur : Antoine Roulin
- * Date : 14.03.2020
- * Version : V0.1
+ * Date : 01.04.2020
+ * Version : V1.0
  * Description : Permet de jouer a la bataille navale et de pouvoir afficher l'aide du jeu
  */
 
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 // fonction qui permet d'afficher l'aide du jeu
 void afficherMenu();
 void aideJeu();
 void jeu();
+void login();
+void chercherFicher();
+char user();
+void writeLog();
+void writeScore();
+void afficherScores();
+void chargementMap();
+void changerEtatEnCoule();
 
 int main() {
     // ligne qui permet au programme de comprendre l'UTF-8
     SetConsoleOutputCP(65001);
+
+    writeLog("Application ouverte");
 
     afficherMenu();
 
@@ -28,15 +41,16 @@ void afficherMenu(){
 
     // déclaration et initialisation des variables
     int choixMenu = 0;
+    char nomJoueur[21] = "Invite";   // nom de l'utilisateur (par défaut le nom est Invité sans le "é" car cela fait bugger l'affichage (j'en ai laissé un avec le "é" pour illustrer le bug d'affichage))
 
     // boucle qui permet de répéter les actions à l'intérieur tant que la condition de sortie n'est pas respecter
     do{
-
         // efface le terminal
         system("@cls||clear");  //source: https://stackoverflow.com/a/33450696
 
         // affichage d'un menu de séléction d'action
         printf("Menu\n\n");
+        printf("Bienvenue %s\n\n", nomJoueur);
         printf(" 1)  Login\n");
         printf(" 2)  Jouer à la bataille navale\n");
         printf(" 3)  Afficher les scores précédants\n");
@@ -46,20 +60,21 @@ void afficherMenu(){
 
         // scan et stock la réponse dans la variable choixMenu
         scanf("%d", &choixMenu);
+
         switch (choixMenu) {
             case 1:
-                printf("Vous avez choisi de vous login, cette fonction n'est pas encore implémentée\n\nAppuyez sur une touche pour quitter le programme");
-                _getch();
+                //printf("Vous avez choisi de vous login, cette fonction n'est pas encore implémentée\n\nAppuyez sur une touche pour quitter le programme");
+                login(nomJoueur);
                 break;
 
             case 2:
                 getchar(); // si on rejoue après avoir gagné erreur passe le premier scanf après le premier passage de la boucle: https://stackoverflow.com/a/9562355
-                jeu();
+                writeLog("Début du jeu");
+                jeu(nomJoueur);
                 break;
 
             case 3:
-                printf("Vous avez choisi d'afficher les scores précédants, cette fonction n'est pas encore implémentée\n\nAppuyez sur une touche pour revenir au menu");
-                _getch();
+                afficherScores();
                 break;
 
             case 4:
@@ -68,6 +83,7 @@ void afficherMenu(){
 
             case 5:
                 printf("Fermeture du jeu.\n");
+                writeLog("Application fermer");
                 break;
 
                 // cela donne une suite d'actionà effectuer si la réponse ne correspond pas à une action proposée
@@ -78,7 +94,7 @@ void afficherMenu(){
         }
 
         // condition pour rester dans la boucle
-    }while(choixMenu < 0 || choixMenu >= 4);
+    }while(choixMenu != 5);
 }
 
 void aideJeu(){
@@ -104,11 +120,11 @@ void aideJeu(){
     _getch();
 }
 
-void jeu(){
+void jeu(char nomUser[21]){
 
     // déclaration des constantes
-    const int LARGEUR = 10; // colonnes
-    const int HAUTEUR = 10; // lignes
+    const int LARGEUR = 10; // colonnes (lettre)
+    const int HAUTEUR = 10; // lignes (nombre)
     const int A_ASCII = 65; // code du A en ASCII
 
     // déclaration des variables
@@ -118,57 +134,30 @@ void jeu(){
     int entreeColonneInt = 0;   // colonne entrée par l'utilisateur traduit en numéro
     int jeuEnCours = 1;         // valeur qui défini si le jeu est en cours ou non
     int message = -1;           // définir quelle message afficher
+    int compteur = 0;           // compteur permetent de compter les scores
+    int etat = 0;               // état de la case
 
-    // initialisation du tableau
-    // états:
-    //  0 = Rien
-    //  1 = Bateau touché
-    //  2 = Bateau coulé
-    //  3 = Bateau non découvert
-    //  4 = Eau
-    for(int h = 0; h < HAUTEUR; h++){
-        for(int l = 0; l < LARGEUR; l++){
-            grille[l][h] = 0;
-        }
-    }
 
-    // placement des bateaux
-    // porte-avion 5 cases
-    grille[1][0] = 3;
-    grille[2][0] = 3;
-    grille[3][0] = 3;
-    grille[4][0] = 3;
-    grille[5][0] = 3;
+    int porteAvionCount = 0;
+    int croiseurCount = 0;
+    int contreCroiseurCount = 0;
+    int sousMarinCount = 0;
+    int torpilleurCount = 0;
+
+
     int porteAvionCoule = 0;
-
-    // croiseur 4 cases
-    grille[0][9] = 3;
-    grille[1][9] = 3;
-    grille[2][9] = 3;
-    grille[3][9] = 3;
     int croiseurCoule = 0;
-
-    // contre-croiseur 3 cases
-    grille[3][4] = 3;
-    grille[3][5] = 3;
-    grille[3][6] = 3;
     int contreCroiseurCoule = 0;
+    int sousMarinCoule = 0;
+    int torpilleurCoule = 0;
 
-    // sous-Marin 3 cases
-    grille[7][2] = 3;
-    grille[7][3] = 3;
-    grille[7][4] = 3;
-    int sousMarin = 0;
-
-    // torpilleur 2 cases
-    grille[7][7] = 3;
-    grille[8][7] = 3;
-    int torpilleur = 0;
+    chargementMap(HAUTEUR, LARGEUR, A_ASCII, grille);
 
     // affichage des case du jeu avec les différentes couleurs
     do{
 
         system("@cls||clear");  // efface l'affichage pour reafficher
+
         printf("Bataille navale\n\n");
 
         // légende
@@ -193,6 +182,11 @@ void jeu(){
 
                 switch (grille[l][h]) {
                     case 1: // bateau touché
+                    case 9:
+                    case 10:
+                    case 11:
+                    case 12:
+                    case 13:
                         printf("\033[0;31m"); // couleur rouge
                         break;
 
@@ -200,13 +194,9 @@ void jeu(){
                         printf("\033[0;32m"); // couleur verte
                         break;
 
-                    case 4: // eau
+                    case 3: // eau
                         printf("\033[0;36m"); // couleur bleu
                         break;
-
-                    /*case 3:  // cheat
-                        printf("\033[0;35m"); // couleur magenta
-                        break;*/
 
                     default:
                         printf("\033[0;37m"); // couleur par défaut
@@ -253,25 +243,22 @@ void jeu(){
 
         // boucle d'entrée utilisateur
         do{
-
             // boucle qui empêche les coordonnée en dehors de la grille de jeu
             do{
-
                 printf("\nQuelle case veux-tu attaquer ?\nLettre de colonne: ");
                 scanf("%c", &entreeColonne);
                 if(!(entreeColonne >= A_ASCII && entreeColonne <= A_ASCII + LARGEUR)){
-                    printf("La coordonnée n'est pas sur le tableau ! Veuillez rentrer une donnée valide");
+                    printf("\nLa coordonnée n'est pas sur le tableau, veuillez rentrer une donnée valide ");
                 }
                 getchar(); // erreur passe le premier scanf après le premier passage de la boucle: https://stackoverflow.com/a/9562355
             } while(!(entreeColonne >= A_ASCII && entreeColonne <= A_ASCII + LARGEUR));
 
             // boucle qui empêche les coordonnée en dehors de la grille de jeu
             do{
-
                 printf("Numéro de ligne: ");
                 scanf("%d", &entreeLigne);
                 if(!(entreeLigne >=1 && entreeLigne <= HAUTEUR)){
-                    printf("La coordonnée n'est pas sur le tableau ! Veuillez rentrer une donnée valide");
+                    printf("\nLa coordonnée n'est pas sur le tableau, veuillez rentrer une donnée valide ");
                 }
                 getchar();
             } while(!(entreeLigne >= 1 && entreeLigne <= HAUTEUR));
@@ -280,65 +267,301 @@ void jeu(){
             entreeColonneInt = entreeColonne - A_ASCII;
 
             // boucle qui empêche de tirer sur une case déjà touchée
-        } while(grille[entreeColonneInt][entreeLigne - 1] == 1 || grille[entreeColonneInt][entreeLigne - 1] == 2 || grille[entreeColonneInt][entreeLigne - 1] == 4);
+        } while(grille[entreeColonneInt][entreeLigne - 1] == 1 || grille[entreeColonneInt][entreeLigne - 1] == 2 || grille[entreeColonneInt][entreeLigne - 1] == 3);
+
+        // ajoute 1 au compteur actuel
+        compteur += 1;
+
+        // message log
+        char messageLog[100] = "Tir sur la case : ";
+        sprintf(messageLog, "%s%c%d", messageLog, entreeColonne, entreeLigne);
+        writeLog(messageLog);
 
         // conditions de touché
-        if(grille[entreeColonneInt][entreeLigne - 1] == 3){
-            grille[entreeColonneInt][entreeLigne - 1] = 1;
-            message = 1;
-        } else { // loupé
-            grille[entreeColonneInt][entreeLigne - 1] = 4;
-            message = 0;
+        switch (grille[entreeColonneInt][entreeLigne - 1]){
+
+            case 0:
+                grille[entreeColonneInt][entreeLigne - 1] = 3;
+                message = 0;
+                writeLog("Loupé !");
+                break;
+
+            case 4:
+                porteAvionCount++;
+                grille[entreeColonneInt][entreeLigne - 1] = 9;
+                message = 1;
+                writeLog("Touché !");
+                break;
+
+            case 5:
+                croiseurCount++;
+                grille[entreeColonneInt][entreeLigne - 1] = 10;
+                message = 1;
+                writeLog("Touché !");
+                break;
+
+            case 6:
+                contreCroiseurCount++;
+                grille[entreeColonneInt][entreeLigne - 1] = 11;
+                message = 1;
+                writeLog("Touché !");
+                break;
+
+            case 7:
+                sousMarinCount++;
+                grille[entreeColonneInt][entreeLigne - 1] = 12;
+                message = 1;
+                writeLog("Touché !");
+                break;
+
+            case 8:
+                torpilleurCount++;
+                grille[entreeColonneInt][entreeLigne - 1] = 13;
+                message = 1;
+                writeLog("Touché !");
+                break;
+
+            case 1:
+            case 2:
+            case 3:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            default:
+                printf("\nUn tir a déjà été effectuer sur cette coordonné\n");
+                break;
         }
+
+        //printf("%d", porteAvionCount);
+        //_getch();
 
         // conditions de coulé
-        if(grille[1][0] == 1 && grille[2][0] == 1 && grille[3][0] == 1 && grille[4][0] == 1 && grille[5][0] == 1){
-            grille[1][0] = 2;
-            grille[2][0] = 2;
-            grille[3][0] = 2;
-            grille[4][0] = 2;
-            grille[5][0] = 2;
-            porteAvionCoule = 1;
+        if(porteAvionCount == 5){
+            etat = 9;
+            changerEtatEnCoule(etat, HAUTEUR, LARGEUR, grille);
             message = 2;
+            writeLog("Porte avion coulé");
+            porteAvionCount = 0;    // évite que le log s'écrive a chaque tour du while et que le message pour l'utilisateur soie supprimer et remplacer par un autre
+            porteAvionCoule = 1;    // permet que le compteur soie remis a zero pour éviter les logs et messages pour l'utilisateur excessif ou faux
         }
 
-        if(grille[0][9] == 1 && grille[1][9] == 1 && grille[2][9] == 1 && grille[3][9] == 1){
-            grille[0][9] = 2;
-            grille[1][9] = 2;
-            grille[2][9] = 2;
-            grille[3][9] = 2;
-            croiseurCoule = 1;
+        if(croiseurCount == 4){
+            etat = 10;
+            changerEtatEnCoule(etat, HAUTEUR, LARGEUR, grille);
             message = 3;
+            writeLog("Croiseur coulé");
+            croiseurCount = 0;
+            croiseurCoule = 1;
         }
 
-        if(grille[3][4] == 1 && grille[3][5] == 1 && grille[3][6] == 1){
-            grille[3][4] = 2;
-            grille[3][5] = 2;
-            grille[3][6] = 2;
-            contreCroiseurCoule = 1;
+        if(contreCroiseurCount == 3){
+            etat = 11;
+            changerEtatEnCoule(etat, HAUTEUR, LARGEUR, grille);
             message = 4;
+            writeLog("Contre-Croiseur coulé");
+            contreCroiseurCount = 0;
+            contreCroiseurCoule = 1;
         }
 
-        if(grille[7][2] == 1 && grille[7][3] == 1 && grille[7][4] == 1){
-            grille[7][2] = 2;
-            grille[7][3] = 2;
-            grille[7][4] = 2;
-            sousMarin = 1;
+        if(sousMarinCount == 3){
+            etat = 12;
+            changerEtatEnCoule(etat, HAUTEUR, LARGEUR, grille);
             message = 5;
+            writeLog("Sous-Marin coulé");
+            sousMarinCount = 0;
+            sousMarinCoule = 1;
         }
 
-        if(grille[7][7] == 1 && grille[8][7] == 1){
-            grille[7][7] = 2;
-            grille[8][7] = 2;
-            torpilleur = 1;
+        if(torpilleurCount == 2){
+            etat = 13;
+            changerEtatEnCoule(etat, HAUTEUR, LARGEUR, grille);
             message = 6;
+            writeLog("Torpilleur coulé");
+            torpilleurCount = 0;
+            torpilleurCoule = 1;
         }
 
         // vérifier les conditions de victoire
-        if(porteAvionCoule == 1 && croiseurCoule == 1 && contreCroiseurCoule == 1 && sousMarin == 1 && torpilleur == 1){
-            printf("\nBravo ! vous avez gagné !\n\nAppuyez sur une touche pour revenir au menu");
+        if(porteAvionCoule == 1 && croiseurCoule == 1 && contreCroiseurCoule == 1 && sousMarinCoule == 1 && torpilleurCoule == 1){
+            printf("\nBravo %s ! Tu as gagné en %d coups !\n\nAppuyez sur une touche pour revenir au menu", nomUser, compteur);
+
+            // message log
+            sprintf(messageLog, "%s%s%d%s", nomUser, " a gagné en ", compteur, " coups");
+            writeLog(messageLog);
+
+            // message score
+            writeScore(nomUser, compteur);
+
             _getch();
             jeuEnCours = 0;
         }
     } while(jeuEnCours == 1);
 }
+
+void login(char nom[]){
+
+    // efface l'affichage
+    system("@cls||clear");
+
+    // affichage informatif
+    printf("Login\n\n");
+
+    // demande a l'utilisateur de rentrer un nom d'utilisateur
+    printf("Entrez un nom d'utilisateur (Le nom doit pas contenir d'espace et faire max 20 caractères) : ");
+
+    // scan la réponse et le mets dans le tableau de caractère
+    scanf("%s", nom);
+
+    // printf la chaine de caractère
+    printf("Ton nom est : %s \n\n", nom);
+
+    // log
+    char message[100] = "L'utilisateur s'est connecté en tant que : ";
+    sprintf(message, "%s%s", message, nom);
+    writeLog(message);
+
+    // les choix possibles après la lecture des règles
+    printf("Appuyer sur une touche pour revenir au menu\n");
+
+    // scan et stock le résultat entrer
+    _getch();
+}
+
+void writeLog(char logMessage[]){
+
+    // ajouter la date au message https://stackoverflow.com/a/10917605
+    char dateText[100];
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    strftime(dateText, sizeof(dateText)-1, "%d.%m.%Y %H:%M", t);
+
+    // écrire le message dans le fichier logs.txt
+    FILE *fL;
+    fL = fopen("../logs/logs.txt", "a+");
+    fprintf(fL, "%s - %s\n", dateText, logMessage);
+    fclose(fL);
+}
+
+void writeScore(char nomUser[21], int saveScore){
+
+    // écrire le message dans le fichier score.txt
+    FILE *fS;
+    fS = fopen("../logs/score.txt", "a+");
+    fprintf(fS, "%-20s || %d\n", nomUser, saveScore);
+    fclose(fS);
+}
+
+void afficherScores(){
+
+    // déclaration de la variable caractere
+    char caractere;
+
+    system("@cls||clear");
+
+    // affichage de l'entête
+    printf("Scores\n\n");
+    printf("%-20s || Score\n", "Nom");
+    printf("=============================\n");
+
+    // affichage des scores
+    FILE *fileScore;
+    fileScore = fopen("../logs/score.txt", "r");
+
+    // boucle qui tourne tant qu'il y a quelque chose dans le fichier
+    while((caractere = fgetc(fileScore)) != EOF){
+        printf("%c", caractere);
+    }
+
+    fclose(fileScore);
+
+    printf("\n\nAppuyer sur une touche pour revenir au menu\n");
+
+    _getch();
+}
+
+void chargementMap(const int HAUTEUR, const int LARGEUR, const int A_ASCII,int grille[][LARGEUR]) {
+
+    // initialisation du tableau
+    // états:
+    //  0 = Rien
+    //  1 = Bateau touché
+    //  2 = Bateau coulé
+    //  3 = Eau
+    //  4 = porteAvion
+    //  5 = croiseur
+    //  6 = contre-Croiseur
+    //  7 = sous-Marin
+    //  8 = torpilleur
+    for (int h = 0; h < HAUTEUR; h++) {
+        for (int l = 0; l < LARGEUR; l++) {
+            grille[l][h] = 0;
+        }
+    }
+
+
+    // scan le fichier ligne par ligne et mets la ligne dans une variable https://stackoverflow.com/a/39237609
+    FILE *fileMaps;
+
+    const int MIN = 1;
+    const int MAX = 6;
+
+    int bufferLength = 255;
+    char buffer[bufferLength];
+    char parametre[16] = "";
+    char valeurH[16] = "";
+    char valeurLS[16] = "";
+    char valeurL;
+    int valeurLInt = 0;
+    int valeurHInt = 0;
+    char map[30] = "";
+    int nombreRandom = 0;
+
+    // génére un nombre aléatoire entre 1 et 6
+    srand(time(NULL));
+    nombreRandom = (rand() % (MAX - MIN + 1)) + MIN;
+
+    sprintf(map, "../maps/map%d.txt", nombreRandom);
+    
+    fileMaps = fopen(map, "r");
+
+    //../maps/map6.txt
+
+    while (fgets(buffer, bufferLength, fileMaps)) {
+
+        sscanf(buffer, "%15s : %15s %15s", parametre, valeurLS, valeurH);
+        valeurL = valeurLS[0];
+        valeurLInt = valeurL - A_ASCII;
+        valeurHInt = atoi(valeurH) - 1; //atoi(...) permet de transformer un string en int      https://stackoverflow.com/questions/7021725/how-to-convert-a-string-to-integer-in-c
+
+
+        if (strcmp(parametre, "porteAvion") == 0) { // https://www.programiz.com/c-programming/library-function/string.h/strcmp
+            grille[valeurLInt][valeurHInt] = 4;
+        } else if (strcmp(parametre, "croiseur") == 0) {
+            grille[valeurLInt][valeurHInt] = 5;
+        } else if (strcmp(parametre, "contre-Croiseur") == 0) {
+            grille[valeurLInt][valeurHInt] = 6;
+        } else if (strcmp(parametre, "sous-Marin") == 0) {
+            grille[valeurLInt][valeurHInt] = 7;
+        } else if (strcmp(parametre, "torpilleur") == 0) {
+            grille[valeurLInt][valeurHInt] = 8;
+        }
+
+    }
+
+    fclose(fileMaps);
+
+}
+
+void changerEtatEnCoule(int etat, const int HAUTEUR, const int LARGEUR, int grille[][LARGEUR]){
+    for (int h = 0; h < HAUTEUR; h++) {
+        for (int l = 0; l < LARGEUR; l++) {
+            if (grille[l][h] == etat){
+                grille[l][h] = 2; // change l'état de touché à coulé
+            }
+        }
+    }
+}
+
+//todo vérifier le nom de toute les variable si ils sont claires
